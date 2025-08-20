a7cd248ede58dcc3f576919ea0c8de8072c456bd
Shruti Badrish <63464583+agkistrodon@users.noreply.github.com>
2025-08-19 22:21:24 -0700

    kernel wip

--- fe2o3/src/main.rs
+++ fe2o3/src/main.rs
@@ -1,14 +1,26 @@
 #![no_std]
-#![no_main]
-
-// entry point
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+#![no_main
+]
 
 // panic
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+// entry point
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
